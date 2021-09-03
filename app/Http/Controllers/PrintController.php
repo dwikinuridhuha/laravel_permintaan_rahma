8bf3335b38b5b2b85440a546fe8a08081795836a
dwikinuridhuha <dwiki.nuridhuha@gmail.com>
2021-09-03 13:41:43 +0700

    print controller, style.css, and print.blade.php changge

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -9,8 +9,11 @@ use App\Models\Petition;
 class PrintController extends Controller
 {
     public function lastInputPrint() {
+        setlocale (LC_TIME, 'id_ID');
+
         $printData = Petition::orderBy('id', 'desc')->first();
-//        dd($printData->nama);
-        return view('print', ['tampil' => $printData]);
+        $tanggalSekarang = date("d F Y");
+
+        return view('print', ['tampil' => $printData, 'tanggalSekarang' => $tanggalSekarang]);
     }
 }
--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -3,15 +3,88 @@
 @section('title', 'Page Print')
 
 @section('content')
-    <div class="container">
-        <h1>{{$tampil->nama}}</h1>
-        <h1>{{$tampil->alamat}}</h1>
-        <h1>{{$tampil->hp}}</h1>
-        <h1>{{$tampil->jabatan}}</h1>
-        <h1>{{$tampil->keperluan}}</h1>
+    <div class="container mt-5">
+        <h3 class="text-center text-uppercase">
+            <u>
+                surat permohonan
+            </u>
+        </h3>
+
+        <div class="my-2">
+            Yang bertanda tanggan di bawah ini :
+        </div>
+
+        <div class="row">
+            <div class="col-1 text-capitalize">
+                nama
+            </div>
+            <div class="col-11">
+                <h6 class="text-capitalize font-weight-normal"><span class="mr-2">:</span>{{$tampil->nama}}</h6>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-1 text-capitalize">
+                alamat
+            </div>
+            <div class="col-11">
+                <h6 class="text-capitalize font-weight-normal"><span class="mr-2">:</span>{{$tampil->alamat}}</h6>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-1 text-capitalize">
+                No HP
+            </div>
+            <div class="col-11">
+                <h6 class="text-capitalize font-weight-normal"><span class="mr-2">:</span>{{$tampil->hp}}</h6>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-1 text-capitalize">
+                Jabatan/Status
+            </div>
+            <div class="col-11">
+                <h6 class="text-capitalize font-weight-normal"><span class="mr-2">:</span>{{$tampil->jabatan}}</h6>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-1 text-capitalize">
+                <span>Keperluan</span>
+            </div>
+
+            <div class="col-11">
+                <h6 class="text-capitalize font-weight-normal"><span class="mr-2">:</span></h6>
+            </div>
+
+            <div class="col-12 ml-3">
+                <h6 class="text-capitalize font-weight-normal">{{$tampil->keperluan}}</h6>
+            </div>
+        </div>
+
+        <div class="row mt-2">
+            <div class="col-12">
+                <h6 class="text-capitalize font-weight-normal text-right">Jombang, {{ $tanggalSekarang }}</h6>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-6">
+                <h6 class="text-capitalize font-weight-normal text-center">Yang Menerima</h6>
+
+                <div class="tanda-tanggan"></div>
+            </div>
+
+            <div class="col-6">
+                <h6 class="text-capitalize font-weight-normal text-center">Hormat Kami</h6>
+                <div class="tanda-tanggan"></div>
+            </div>
+        </div>
     </div>
 
     <script>
-        window.print();
+        // window.print();
     </script>
 @endsection
